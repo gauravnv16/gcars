d3afb8674c62dfa6c5727af9a458937cce7a8c2c
GAURAV N V <114969570+gauravnv16@users.noreply.github.com>
2024-02-13 22:27:48 +0530

    bug-fix:width-overflow-issue-fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <Header />
       <aside
         className="fixed right-1 bottom-5"
-        style={{
-          boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-        }}
+        // style={{
+        //   boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+        // }}
       >
         <section className="flex justify-center items-center bg-blue-500 w-fit p-4 rounded-full my-2 ">
           <i className="fa-solid fa-envelope text-white text-2xl" />
